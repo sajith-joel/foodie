b08b64e1a0620d7ae53fc28b5094e13df72a16ce
sajith-joel <joelsajith25@gmail.com>
2026-03-12 15:44:24 +0530

    Initial commit12

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -93,7 +93,7 @@ const Login = () => {
           </p>
         </form>
 
-        <div className="mt-6">
+        {/* <div className="mt-6">
           <div className="relative">
             <div className="absolute inset-0 flex items-center">
               <div className="w-full border-t border-gray-300"></div>
@@ -101,7 +101,7 @@ const Login = () => {
             <div className="relative flex justify-center text-sm">
               <span className="px-2 bg-transparent text-gray-200">Demo Credentials</span>
             </div>
-          </div>
+          </div> */}
 
           {/* <div className="mt-6 grid grid-cols-2 gap-3 text-xs text-gray-200">
             <div className="bg-white bg-opacity-20 p-2 rounded">
